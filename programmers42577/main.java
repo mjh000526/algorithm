e8f7bf5b94ddc57f98a0b749d82d4ab4ed64eb86
mjh000526 <mjh000526@gmail.com>
2025-01-10 15:17:41 +0900

    programmers42577

--- programmers42577/main.java
+++ programmers42577/main.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class main {
     public static void main(String[] args){
-        String[] phone_book = {"119", "97674223", "1195524421"};
-        Arrays.sort(phone_book);
-        for(int i=1;i<phone_book.length;i++){
-            if (phone_book[i].startsWith(phone_book[i - 1])) {
-                System.out.println("false");
-            }
+        String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+        HashMap<String, Integer> map = new HashMap<>();
+        int sum=1;
+        for(int i=0;i<clothes.length;i++){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],1) + 1);
         }
-        System.out.println("true");
+        List<String> keySet = new ArrayList<>(map.keySet());
+        for(int i=0;i<keySet.size();i++){
+            sum*=map.get(keySet.get(i));
+        }
+        System.out.println(sum-1);
     }
 }
