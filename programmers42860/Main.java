ff406233481a10bd86e52704c539bdf4ae7af950
mjh000526 <mjh000526@gmail.com>
2025-01-21 22:44:55 +0900

    programmer43105

--- programmers42860/Main.java
+++ programmers42860/Main.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    static public void main(String[] args){
+        String name = "JEROEN";
+        System.out.println(solution(name));
+    }
+
+    static public int solution(String name) {
+        int answer = 0;
+        int cnt = 1;
+        boolean[] visit = new boolean[name.length()];
+
+        for(int i=0;i<name.length();i++){
+            int c = name.charAt(i) - 65;
+            answer += Math.min(c,25-c+1);
+//            System.out.println((char)(c+65)+"("+c + ") : "  + Math.min(c,25-c+1));
+            if(c==0) {
+                cnt++;
+                visit[i]=true;
+            }
+        }
+
+        visit[0] = true;
+
+        int cursor = 0;
+        while (cnt<name.length()){
+            int i = cursor;
+            int j = cursor;
+            int front = 0;
+            int back = 0;
+            while(visit[i]){
+                i++;
+                front++;
+                if(i==name.length()){
+                    i=0;
+                }
+//                System.out.println("처음" + i + ", visit : " + visit[i] + ", char : " + name.charAt(i) + "cnt" + cnt);
+            }
+            while(visit[j]){
+                j--;
+                back++;
+                if(j==-1){
+                    j=name.length()-1;
+                }
+            }
+            if(back<front){
+                cursor=j;
+                answer+=back;
+                visit[j]=true;
+//                System.out.println("back");
+            }
+            else {
+                cursor=i;
+                answer+=front;
+                visit[i]=true;
+//                System.out.println("front : " + front);
+            }
+            cnt++;
+        }
+
+        return answer;
+    }
 }
--- programmers42862/Main.java
+++ programmers42862/Main.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 5;
+        int[] lost = {5,3};
+        int[] reserve = {4,2};
+        System.out.println(solution(n,lost,reserve));
+    }
+
+    static public int solution(int n, int[] lost, int[] reserve) {
+        int[] student = new int[n+2];
+        int answer = 0;
+
+        for(int i=0;i<lost.length;i++){
+            student[lost[i]]-=1;
+        }
+        for(int i=0;i<reserve.length;i++){
+            student[reserve[i]]+=1;
+        }
+        for(int i=1;i<=n;i++){
+            if(student[i-1]==-1&&student[i]==1){
+                student[i-1]++;
+                student[i]--;
+            } else if (student[i+1]==-1&&student[i]==1) {
+                student[i+1]++;
+                student[i]--;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(student[i]>-1) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
